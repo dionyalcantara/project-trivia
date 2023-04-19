500f9a63c18cd15b0ec6fddec58e6aee9607cb60
Rfael-nasm <alysson-242@live.com>
2023-04-19 16:38:03 -0300

    req1 completo

---
+++ src/pages/login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export class Login extends React.Component {
+  state = {
+    name: '',
+    email: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { email, name } = this.state;
+
+    const isValid = name.length > 0 && email.length > 0;
+
+    return (
+      <main>
+        <form>
+          <label htmlFor="name-id">Nome</label>
+          <input
+            type="text"
+            id="name-id"
+            value={ name }
+            onChange={ this.handleChange }
+            name="name"
+            data-testid="input-player-name"
+          />
+
+          <label htmlFor="email-id">Email</label>
+          <input
+            type="text"
+            id="email-id"
+            value={ email }
+            onChange={ this.handleChange }
+            name="email"
+            data-testid="input-gravatar-email"
+          />
+
+          <button
+            type="submit"
+            disabled={ !isValid }
+            data-testid="btn-play"
+          >
+            Play
+          </button>
+        </form>
+      </main>
+    );
+  }
+}
