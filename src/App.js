d2a5c62a791bbf5494346a60a351ebf8eec8550b
Rfael-nasm <alysson-242@live.com>
2023-04-19 17:07:13 -0300

    import corrected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { Login } from './pages/Login';
+import Login from './pages/Login';
 
 export default function App() {
   return (
