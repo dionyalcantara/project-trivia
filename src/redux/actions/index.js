e4ad5e367d14e293a6d328100501a994a02dafb0
dionyalcantara <dionyguape@gmail.com>
2023-04-20 14:47:51 -0300

    Requisito 2 feito

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,36 @@
-// desenvolver action conforme nescessidade
+import * as type from './actionTypes';
+
+export function requestStarted() {
+  return { type: type.REQUEST_STARTED };
+}
+
+export function requestSuccessful(data) {
+  return {
+    type: type.REQUEST_SUCCESSFUL,
+    payload: data,
+  };
+}
+
+export function requestFailed(error) {
+  return {
+    type: type.REQUEST_FAILED,
+    payload: error,
+  };
+}
+
+export function fetchTrivia() {
+  return (dispatch) => {
+    dispatch(requestStarted());
+    fetch('https://opentdb.com/api_token.php?command=request')
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('token', data.token);
+        fetch(`https://opentdb.com/api.php?amount=5&token=${localStorage.getItem('token')}`)
+          .then((responseTwo) => responseTwo.json())
+          .then((dataTwo) => {
+            dispatch(requestSuccessful());
+            console.log(dataTwo);
+          });
+      });
+  };
+}
