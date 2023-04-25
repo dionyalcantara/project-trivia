86d4634715d59702339de6bc83bc74feabd9eef4
Rfael-nasm <alysson-242@live.com>
2023-04-25 08:35:00 -0300

    alterado o mecanismo de redirect para pagina game e req18 completo

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { resetLogin } from '../redux/actions';
 
 class Ranking extends React.Component {
   render() {
+    const { history, dispatch } = this.props;
+
     return (
       <main>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          data-testid="btn-go-home"
+          onClick={ async () => {
+            await dispatch(resetLogin());
+            history.push('/');
+          } }
+        >
+          Inicio
+        </button>
       </main>
     );
   }
 }
 
-export default Ranking;
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Ranking);
