ed7206b60a2ebbc48d65c66a896d72c888a86ef1
dionyalcantara <dionyguape@gmail.com>
2023-04-24 23:39:18 -0300

    Requisito feito

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
+const MAX_ASSERTIONS = 3;
+
 class Feedback extends React.Component {
-  render() { 
-    const { assertions, score} = this.props;
+  render() {
+    const { assertions, score } = this.props;
     return (
       <section>
         <Header />
-        <p data-testid="feedback-total-score">{ `Você acertou ${score} questões` }</p>
-        <p data-testid="feedback-total-question">{ `Um total de ${assertions} pontos` }</p>
+        <h3 data-testid="feedback-text">
+          {
+            assertions < MAX_ASSERTIONS ? 'Could be better...' : 'Well Done!'
+          }
+        </h3>
+        <p data-testid="feedback-total-score">
+          {
+            `Você acertou ${score} questões`
+          }
+        </p>
+        <p data-testid="feedback-total-question">
+          {
+            `Um total de ${assertions} pontos`
+          }
+        </p>
       </section>
-    )
+    );
   }
 }
 
