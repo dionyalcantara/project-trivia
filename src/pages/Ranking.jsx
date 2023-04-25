b560fb7e27531d9821c47104d573aadaba7d6c3c
Rejane Lima <rejane.lima87@gmail.com>
2023-04-25 19:01:59 -0300

    Requsito 19

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,15 +1,36 @@
+import gravatar from 'gravatar';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { resetLogin } from '../redux/actions';
 
 class Ranking extends React.Component {
+  state = {
+    players: [],
+  };
+
+  componentDidMount() {
+    const players = JSON.parse(localStorage.getItem('players')) || [];
+    players.sort((a, b) => b.score - a.score);
+    this.setState({ players });
+  }
+
   render() {
     const { history, dispatch } = this.props;
+    const { players } = this.state;
 
     return (
       <main>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <ol>
+          {players.map((player, index) => (
+            <li key={ player.email }>
+              <img src={ gravatar.url(player.email) } alt={ player.name } />
+              <div data-testid={ `player-name-${index}` }>{player.name}</div>
+              <div data-testid={ `player-score-${index}` }>{player.score}</div>
+            </li>
+          ))}
+        </ol>
         <button
           data-testid="btn-go-home"
           onClick={ async () => {
